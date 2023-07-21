8798b281a88e604c8cf89be8cb955728309ee997
BrenoNovais <breno.novais@hotmail.com>
2023-07-21 11:34:31 -0300

    Configurando ESLint-@rocketseat/eslint-config

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,31 +1,32 @@
-import { css, styled } from "styled-components";
+import { css, styled } from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant,
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secundary: 'orange',
-    danger: 'red',
-    success: 'green'
+  primary: 'purple',
+  secundary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    background-color: ${props => props.theme["green-500"]};
-    color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-
-    /*     ${props => {
-        console.log(buttonVariants[props.variant])
-        return css`background-color: ${buttonVariants[props.variant]}`
-    }} */
+  /*     ${(props) => {
+    console.log(buttonVariants[props.variant])
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }} */
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,12 +10,12 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-sizing: 0 0 0 2px ${props => props.theme['green-500']};
+        box-sizing: 0 0 0 2px ${(props) => props.theme['green-500']};
     }
 
     body {
-        background: ${props => props.theme['gray-900']};
-        color: ${props => props.theme['gray-300']};
+        background: ${(props) => props.theme['gray-900']};
+        color: ${(props) => props.theme['gray-300']};
     }
 
     body, input-security, textarea, button {
