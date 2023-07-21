377fc394b365fb6d4c0367b97bb407115e376b98
BrenoNovais <breno.novais@hotmail.com>
2023-07-21 11:13:40 -0300

    Fix: Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
-import { defaltTheme } from "./styles/themes/default";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaltTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secundary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
