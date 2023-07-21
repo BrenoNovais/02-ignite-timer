adc280ec405b2a6971de71a01d98c28b096726cc
BrenoNovais <breno.novais@hotmail.com>
2023-07-21 10:11:23 -0300

    Styled Components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,24 @@
+import { css, styled } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+    variant: ButtonVariant,
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secundary: 'orange',
+    danger: 'red',
+    success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+
+    ${props => {
+        console.log(buttonVariants[props.variant])
+        return css`background-color: ${buttonVariants[props.variant]}`
+    }}
+`
