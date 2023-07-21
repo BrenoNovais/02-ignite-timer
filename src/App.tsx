7ee3c69017e3ed0bc5c4d5a416be8e174fb4cea1
BrenoNovais <breno.novais@hotmail.com>
2023-07-21 10:23:12 -0300

    Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaltTheme } from "./styles/themes/defalt";
 
 export function App() {
-
   return (
-    <>
+    <ThemeProvider theme={defaltTheme}>
       <Button variant="primary" />
       <Button variant="secundary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/defalt.ts
@@ -0,0 +1,6 @@
+export const defaltTheme = {
+    white: '#FFF',
+    primary: 'purple',
+    secundary: 'orange'
+
+}
