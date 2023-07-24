204a297abe8e78b34d085d16372b408c2957d904
BrenoNovais <breno.novais@hotmail.com>
2023-07-24 15:17:48 -0300

    Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="P1"></option>
+            <option value="P1"></option>
+            <option value="P1"></option>
+            <option value="Potato"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            min={1}
+            max={60}
           />
 
           <span>minutos.</span>
