3cdd8c9cc6e50938bb8dc940111a53bada53d30b
BrenoNovais <breno.novais@hotmail.com>
2023-07-25 15:31:47 -0300

    NewCycleFormData

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,12 +21,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, '60 minutos é o máximo'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
